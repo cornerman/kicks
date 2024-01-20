b78178a10d60f97b33e4b7d7e390cc60f04f1060
johannes karoff <johannes@karoff.net>
2024-01-20 02:55:52 +0100

    wip

---
+++ schema.sql
@@ -0,0 +1,23 @@
+-- This file represents the database schema. You can edit it.
+-- After editing, we create a new migration file with liquibase.
+--
+-- Info:
+-- - PRIMARY KEY needs NOT NULL, otherwise liquibase will oscillate between create/drop not-null-constraint.
+
+CREATE TABLE foo(wolf text);
+
+-- Create Person table
+CREATE TABLE Person (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    name TEXT NOT NULL,
+    age INTEGER,
+    address_id INTEGER,
+    FOREIGN KEY (address_id) REFERENCES Address(id)
+);
+
+-- Create Address table
+CREATE TABLE Address (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    street TEXT,
+    city TEXT NOT NULL
+);
